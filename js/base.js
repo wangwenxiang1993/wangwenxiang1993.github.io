e8d91c9d0a5263238f7656bf25de04b97e041d49
xinying.wwx <xinying.wwx@alibaba-inc.com>
2020-03-28 16:52:39 +0800

    Site updated: 2020-03-28 16:52:38

---
+++ js/base.js
@@ -0,0 +1,50 @@
+var nowPage;
+
+NProgress.configure({ showSpinner: false, minimum: 0.1 });
+NProgress.start();
+    $(window).load(function() {
+    NProgress.done(true);
+    $('.fade').removeClass('out');
+    nowPage = location.pathname
+});
+
+$(".guide-button,.extend").click(function () {
+    var page = $(this).attr("_href");
+    if (page == nowPage) {
+        $("body").animate({scrollTop:"0px"},600);
+    }else {
+        nowPage = page;
+        location.href = $(this).attr("_href");
+    }
+});
+
+$(".book-post").click(function () {
+    location.href = $(this).attr("_href");
+});
+
+$("#info-github").click(function(){
+    window.open($(this).attr("_href"));
+});
+
+$(".post .con img").click(function () {
+    var url = $(this).attr("src");
+    var img = $(".img-view span img")
+    var h = $(window).height()-20;
+    var w = $(window).width()-40;
+    img.attr("src",url);
+    img.css({"max-height": h+"px"});
+    img.css({"max-width":w+"px"});
+    $(".img-view").css("display","table");
+});
+
+$(".img-view").click(function () {
+    $(".img-view").css("display","none");
+});
+
+$(".show-extend").mouseover(function () {
+    $("#tech-extend").css("display", "block")
+});
+
+$(".show-extend").mouseleave(function () {
+    $("#tech-extend").css("display", "none")
+});
